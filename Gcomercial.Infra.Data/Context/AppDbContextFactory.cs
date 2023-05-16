4836e29653b3d91226b063ebc036f7cb6a1aa411
GabrielInacio03 <inaciogs03@gmail.com>
2023-05-16 05:16:19 -0300

    finalizado de todos os passos para iniciar os cruds e as demais funcionalidades

--- Gcomercial.Infra.Data/Context/AppDbContextFactory.cs
+++ Gcomercial.Infra.Data/Context/AppDbContextFactory.cs
@@ -13,8 +13,11 @@ namespace Gcomercial.Infra.Data.Context
 		public AppDbContext CreateDbContext(string[] args)
 		{
 			var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
-			optionsBuilder.UseSqlServer("Data Source=BAZINGA\\SQLEXPRESS;Initial Catalog=DBGcomercial;Integrated Security=True");
-			return new AppDbContext(optionsBuilder.Options);
+			optionsBuilder.UseSqlServer("Data Source=BAZINGA\\SQLEXPRESS;Initial Catalog=DBGcomercial;Integrated Security=True"); //BAZINGA\\SQLEXPRESS
+			//BAZINGA\\Inacio
+            //private const string CONNECTIONSTRING = "Password=123@cetec;Persist Security Info=True;User ID=usr_heitor;Initial Catalog=TreinamentoH;Data Source=bd.ceteclins.com.br";
+
+            return new AppDbContext(optionsBuilder.Options);
 		}
 	}
 }
---
+++ Gcomercial.Web.Mvc/Controllers/ProdutosController.cs
@@ -0,0 +1,26 @@
+﻿using Gcomercial.Application.Interfaces;
+using Gcomercial.Application.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Gcomercial.Web.Mvc.Controllers
+{
+	public class ProdutosController : Controller
+	{
+		private IProdutoService _produtoService;
+
+		public ProdutosController(IProdutoService produtoService)
+		{
+			_produtoService = produtoService;
+		}
+		public IActionResult Index()
+		{
+			ProdutoViewModel model = _produtoService.GetProdutos();
+
+			foreach (var item in model.Produtos)
+			{
+                Console.WriteLine("item -> "+ item.Nome);
+            }
+			return View(model);
+		}
+	}
+}
